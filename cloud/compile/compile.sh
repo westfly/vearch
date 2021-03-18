155d70d20cec17c9ce695789c810d24072e920b1
aresyang <yweiyun@163.com>
2021-03-18 13:47:14 +0800

    make version with varible

--- cloud/compile/compile.sh
+++ cloud/compile/compile.sh
@@ -14,9 +14,23 @@ cd /env/app/go/src/github.com/vearch/vearch/build
 ./build.sh
 
 mkdir -p /vearch/build/lib/
+function copy_deps() {
+	# copy from https://h3manth.com/content/copying-shared-library-dependencies
+	[[ ! -e $1 ]] && echo "Not a vaild input $1" && exit 1
+	[[ -d $2 ]] || echo "No such directory $2 creating..."&& mkdir -p "$2"
+	#Get the library dependencies
+	echo "Collecting the shared library dependencies for $1..."
+	deps=$(ldd $1 | awk 'BEGIN{ORS=" "}$1~/^\//{print $1}$3~/^\//{print $3}' | sed 's/,$/\n/')
+	echo "Copying the dependencies to $2"
+	#Copy the deps
+	for dep in $deps
+	do
+		echo "Copying $dep to $2"
+		cp "$dep" "$2"
+	done
+	echo "Done!"
 
-cp /env/app/rocksdb_install/lib/librocksdb.* /vearch/build/lib/
-cp /env/app/zfp_install/lib/libzfp.* /vearch/build/lib/
-cp /vearch/build/gamma_build/libgamma.* /vearch/build/lib/
-
+}
+export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:/vearch/build/gamma_build
+copy_deps /vearch/build/bin/vearch /vearch/build/lib
 rm -rf /vearch/build/gamma_build
--- cloud/env/env.sh
+++ cloud/env/env.sh
@@ -3,58 +3,48 @@
 yum update
 yum install -y epel-release
 yum install -y wget gcc gcc-c++ make automake git blas-devel lapack-devel which openssl-devel libzstd-devel openblas-devel
-if [ ! -d "/env/app" ]; then
-  mkdir -p /env/app
+deploy_home=/env/app
+if [ ! -d $deploy_home ]; then
+  mkdir -p $deploy_home
 fi
-cd /env/app/
-if [ ! -f "cmake-3.20.0-rc3.tar.gz" ]; then
-    wget https://cmake.org/files/v3.20/cmake-3.20.0-rc3.tar.gz
+zfp_version="0.5.5"
+rocksdb_version="v6.2.2"
+go_version=1.12.7
+cmake_version="3.19.7"
+cmake_linux_x86="cmake-${cmake_version}-Linux-x86_64"
+cmake_linux_tar_file="${cmake_linux_x86}.tar.gz"
+cd $deploy_home
+cmake --version
+if [ $? -ne 0 ]; then
+    wget https://github.com/Kitware/CMake/releases/download/v${cmake_version}/${cmake_linux_tar_file}
+    tar xf $cmake_linux_tar_file -C $deploy_home
+    add_sys_path="export PATH=$deploy_home/${cmake_linux_x86}/bin:$PATH"
+    export_cmake_root="export CMAKE_ROOT=$deploy_home/${cmake_linux_x86}"
+    $add_sys_path
+    $export_cmake_root
+    echo "$export_cmake_root $add_sys_path"
+    echo -e "$make_path\n$export_cmake_root\n" >> "/root/.bashrc"
 fi
-tar xf cmake-3.20.0-rc3.tar.gz
-cd /env/app/cmake-3.20.0-rc3
-./bootstrap
-gmake
-gmake install
-cd /usr/bin
-if [ ! -f "cmake" ]; then
-    ln -s cmake3 cmake
-fi
-
-cd /env/app/
 if [ ! -d "zfp" ]; then
-    wget https://github.com/LLNL/zfp/archive/0.5.5.tar.gz -O zfp.tar.gz
+    wget https://github.com/LLNL/zfp/archive/${zfp_version}.tar.gz -O zfp.tar.gz
     tar -xzvf zfp.tar.gz
-    cd /env/app/zfp-0.5.5
+    cd $deploy_home/zfp-${zfp_version}
     mkdir build && cd build
-    cmake ..
-    cmake --build . --config Release
+    cmake -DCMAKE_INSTALL_PREFIX=$deploy_home/zfp_install ..
+    cmake --build --config Release .
     make install
-    mkdir -p /env/app/zfp_install/lib
-    cd /usr/local/lib64
-    cp libzfp.so.0.5.5 /env/app/zfp_install/lib
-    cd /env/app/zfp_install/lib
-    ln -s libzfp.so.0.5.5 libzfp.so.0
-    ln -s libzfp.so.0 libzfp.so
-    cp -r /usr/local/include /env/app/zfp_install/
 fi
-
 cd /env/app
-if [ ! -f "rocksdb-v6.2.2.tar.gz" ]; then
-    wget https://github.com/facebook/rocksdb/archive/v6.2.2.tar.gz -O rocksdb-v6.2.2.tar.gz
+if [ ! -f "rocksdb-${rocksdb_version}.tar.gz" ]; then
+    wget https://github.com/facebook/rocksdb/archive/${rocksdb_version}.tar.gz -O rocksdb-${rocksdb_version}.tar.gz
 fi
-tar -xzf rocksdb-v6.2.2.tar.gz
-cd /env/app/rocksdb-6.2.2
-make shared_lib
-mkdir -p /env/app/rocksdb_install/lib
-cp librocksdb.so.6.2.2 /env/app/rocksdb_install/lib
-cd /env/app/rocksdb_install/lib
-ln -s librocksdb.so.6.2.2 librocksdb.so.6.2
-ln -s librocksdb.so.6.2 librocksdb.so
-cp -r /env/app/rocksdb-6.2.2/include /env/app/rocksdb_install/
-
-cd /env/app/
-if [ ! -f "go1.12.7.linux-amd64.tar.gz" ]; then
-    wget https://dl.google.com/go/go1.12.7.linux-amd64.tar.gz
+tar -xzf rocksdb-v${rocksdb_version}.tar.gz
+cd $deploy_home/rocksdb-${rocksdb_version}
+make shared_lib -j30
+make install-shared INSTALL_PATH=$deploy_home/rocksdb_install
+cd /$deploy_home
+if [ ! -f "go${go_version}.linux-amd64.tar.gz" ]; then
+    wget https://dl.google.com/go/go${go_version}.linux-amd64.tar.gz
 fi
-tar -xzf go1.12.7.linux-amd64.tar.gz
-
+tar -xzf go${go_version}.linux-amd64.tar.gz
+#rm *tar.gz
